20bb336ee3fcffb558e3a351c0075f4faec3cff6
FRsparrow <1528525449@qq.com>
2018-06-13 23:52:36 +0800

    change code by FRsparrow

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,10 +3,6 @@
 int main(void){
     printf("hello, world\n");
     printf("This is a breakpoint test.");
-    printf("this is first 123");
-    printf("this is first 123");
-    printf("this is first 123");
-    printf("this is first 123");
     getchar();
     return 0;
 }
