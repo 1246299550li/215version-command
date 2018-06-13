70cd926785d9bef5c17a5ab7ad4290efdbf36ec5
1246299550li <1246299550@qq.com>
2018-06-13 22:58:22 +0800

    code by liheyu

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,9 @@ int main(void){
     printf("hello, world\n");
     printf("This is a breakpoint test.");
     printf("this is first 123");
+    printf("this is first 123");
+    printf("this is first 123");
+    printf("this is first 123");
     getchar();
     return 0;
 }
