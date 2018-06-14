34a7e6ae07c9d8f986d5e4fc41b22d6823f96734
1246299550li <1246299550@qq.com>
2018-06-14 14:42:17 +0800

    14:42

--- t1987.cpp
+++ t1987.cpp
@@ -17,68 +17,52 @@
 #include <climits>
 const long long llf = 0x3f3f3f3f3f3f3f3f;
 const int inf = 0x3f3f3f3f;
-const int maxn = 1000005;
+const int maxn = 10005;
 using namespace std;
-
-int pos, ans;
+//字典树
+int tree_num, ans;
+char data[maxn];
 struct node
 {
     int num;
     int child[2];
 } tree[10000010];
-
-int add()
-{
-    pos++;
-    tree[pos].num = 0;
-    for (int i = 0; i < 2; i++)
-    {
-        tree[pos].child[i] = -1;
-    }
-    return pos;
-}
-
-int inser(char *str)
+void inser(char *str)
 {
     int post = 0;
-    int tmp = 0;
     int len = strlen(str);
     for (int i = 0; i < len; i++)
     {
         int m = str[i] - '0';
-        if (tree[post].child[m] == -1)
+        if (tree[post].child[m] == -1)//如果子树为空则创建子树
         {
-            if (tmp == 0)
-                tmp = i + 1;
-            tree[post].child[m] = add();
+            tree_num++;
+            tree[tree_num].num = 0;
+            for (int i = 0; i < 2; i++)
+                tree[tree_num].child[i] = -1;
+            tree[post].child[m] = tree_num;
         }
-
         post = tree[post].child[m];
         tree[post].num++;
         ans = max(ans, tree[post].num * (i + 1));
     }
-    if (!tmp)
-        tmp = len;
-    return tmp;
 }
-
-char arr[maxn];
 int main()
 {
-    freopen("input.in", "r", stdin);
+    // freopen("input.in", "r", stdin);
     int t;
     scanf("%d", &t);
     while (t--)
     {
         int n;
-        pos = 0;
+        tree_num = 0;
         memset(tree[0].child, -1, sizeof(tree[0].child));
         scanf("%d", &n);
         ans = 0;
         for (int i = 0; i < n; i++)
         {
-            scanf("%s", arr);
-            int k = inser(arr);
+            scanf("%s", data);
+            inser(data);
         }
         cout << ans << endl;
     }
---
+++ tz.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <iterator>
+#include <iomanip>
+#include <fstream>
+#include <climits>
+const long long llf = 0x3f3f3f3f3f3f3f3f;
+const int inf = 0x3f3f3f3f;
+const int maxn = 10005;
+using namespace std;
+//字典树
+
+int main()
+{
+    // freopen("input.in", "r", stdin);
+    int n;
+    while (~scanf("%d", &n))
+    {
+        
+    }
+    return 0;
+}
