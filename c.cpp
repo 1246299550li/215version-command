cc607935179759db899448d61b6a4423b381e9bf
1246299550li <1246299550@qq.com>
2018-11-29 10:14:17 +0800

    data

--- c.cpp
+++ c.cpp
@@ -13,25 +13,27 @@
 #include <set>
 const long long llf = 0x3f3f3f3f3f3f3f3f;
 const int inf = 0x3f3f3f3f;
-const int maxn = 100005;
+const int maxn = 105;
 using namespace std;
+int dp[maxn][maxn];
 int main()
 {
 	// freopen("input.in", "r", stdin);
 	// freopen("input.out", "w", stdout);
-	int t;
-	cin >> t;
-	while (t--)
+	int n, data[maxn];
+	while (~scanf("%d", &n))
 	{
-		int n;
-		cin >> n;
-		for (int i = 0; i < n; i++)
-		{
-			int tmp;
-			cin >> tmp;
-			sum[i] = sum[i - 1] + tmp;
-			dp[i][i] = 0;
-		}
+		for (int i = 1; i <= n; i++)
+			scanf("%d", &data[i]);
+		for (int len = 2; len < n; len++)
+			for (int start = 2; start + len - 1 <= n; start++)
+			{
+				int end = start + len - 1;
+				dp[start][end] = inf;
+				for (int k = start; k < end; k++)
+					dp[start][end] = min(dp[start][end], dp[start][k] + dp[k + 1][end] + data[start - 1] * data[k] * data[end]);
+			}
+		printf("%d\n", dp[2][n]);
 	}
 	return 0;
 }
